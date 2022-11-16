2e4ab6aa28a0b7ad6477b130083fc008968a66e2
Lucas Ferreira <lks_ferreira@hotmail.com>
2022-11-16 10:32:20 -0300

    att

---
+++ csharp/beecrowd.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+
+
+    private static void Main(string[] args)
+    {
+
+        /** beecrowd 1001
+
+        static int somarDoisValores(int primeiroValor, int segundoValor)
+        {
+            return primeiroValor + segundoValor;
+        }
+
+        int primeiroValor = int.Parse(Console.ReadLine());
+        int segundoValor = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"X = {somarDoisValores(primeiroValor, segundoValor)}");
+        */
+
+        static double calcularArea(double raio)
+        {
+            return Math.PI * (Math.Pow(raio, 2));
+        }
+
+        double area = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"A={calcularArea(area)}");
+    }
+
+}
