b1fe7b3a3dcfdc72526461d13a50dfeb00d1e562
Lucas Ferreira <lks_ferreira@hotmail.com>
2022-11-24 11:24:17 -0300

    app

--- csharp/beecrowd.cs
+++ csharp/beecrowd.cs
@@ -3,8 +3,194 @@
 
 
     private static void Main(string[] args)
-    {   
+    {
+        string[] arrayValores = Console.ReadLine().Split();
+
+        int valorA = int.Parse(arrayValores[0]);
+        int valorB = int.Parse(arrayValores[1]);
+        int valorC = int.Parse(arrayValores[2]);
+        int valorD = int.Parse(arrayValores[3]);
+
+        if ((valorB > valorC) && (valorD > valorA) && ((valorC + valorD) > (valorA + valorB)) && (valorC > 0) && (valorD > 0) && (valorA % 2 == 0))
+        {
+            System.Console.WriteLine("Valores aceitos");
+        } else
+        {
+            System.Console.WriteLine("Valores nao aceitos");
+        }
+
+
+        /**
+        1021 - Notas e Moedas
+        https://www.beecrowd.com.br/judge/pt/problems/view/1021
         
+
+        // Recebe um valor double do usuário e guarda esse valor na variável valorInserido.
+        double valorInserido = double.Parse(Console.ReadLine());
+
+        // Guarda apenas a parte inteiro do valor double na variável valorNotas.
+        int valorNotas = (int)(valorInserido);
+
+        // Para tabalhar com as moedas iremos subtrair o inserido do valor inteiro e multiplicar por 100 para manipula-lo com inteiro.
+        // Exemplo: R$250,57 - 250 = 0,57 // 0,57 * 100 // 57 -> Agora manipularemos a parte decimal como um inteiro.
+
+        // Lógica por tras da moeda
+
+        // Ao multiplicar o um decimal por 100 podemos tratar o valor como inteiro.
+        // Pense assim: Ao invez de ler uma como como 0,50 de Real, iremos ler ela como 50 centavos.
+        double moedasConvertidas = Math.Floor((valorInserido - valorNotas)*100);
+
+        //Remove todas as possíveis casa do número infinitéssimo gerado na manipulação dos números.
+        int valorMoedas = (int)moedasConvertidas;
+        
+        //Agora replicamos a lógica de cima em pequenos blocos dos valores que queremos obter.
+
+        //Obtem as notas de 100
+        int notas100 = valorNotas/100;
+        valorNotas -= notas100*100;
+        
+        //Obtem as notas de 50
+        int notas50 = valorNotas/50;
+        valorNotas -= notas50*50;
+
+        //Obtem as notas de 20
+        int notas20 = valorNotas/20;
+        valorNotas -= notas20*20;
+
+        //Obtem as notas de 10
+        int notas10 = valorNotas/10;
+        valorNotas -= notas10*10;
+
+        //Obtem as notas de 5
+        int notas5= valorNotas/5;
+        valorNotas -= notas5*5;
+
+        //Obtem as notas de 2
+        int notas2 = valorNotas/2;
+        valorNotas -= notas2*2;
+
+        //Obtem as notas de 1
+        int moeda1 = valorNotas/1;
+        valorNotas -= moeda1*1;
+
+        //Obtem as moedas de 50
+        int moeda50 = valorMoedas/50;
+        valorMoedas -= (moeda50*50);
+
+        //Obtem as moedas de 25
+        int moeda25 = valorMoedas/25;
+        valorMoedas -= moeda25*25;
+
+        //Obtem as moedas de 10
+        int moeda10 = valorMoedas/10;
+        valorMoedas -= moeda10*10;
+
+        //Obtem as moedas de 05
+        int moeda05 = valorMoedas/05;
+        valorMoedas -= (moeda05*05);
+
+        //Obtem as moedas de 1
+        int moeda01 = valorMoedas/1;
+        valorMoedas -= moeda01*1;
+
+        Console.WriteLine("NOTAS:");
+        Console.WriteLine($"{notas100} nota(s) de R$ 100.00");
+        Console.WriteLine($"{notas50} nota(s) de R$ 50.00");
+        Console.WriteLine($"{notas20} nota(s) de R$ 20.00");
+        Console.WriteLine($"{notas10} nota(s) de R$ 10.00");
+        Console.WriteLine($"{notas5} nota(s) de R$ 5.00");
+        Console.WriteLine($"{notas2} nota(s) de R$ 2.00");
+        Console.WriteLine("MOEDAS:");
+        Console.WriteLine($"{moeda1} moeda(s) de R$ 1.00");
+        Console.WriteLine($"{moeda50} moeda(s) de R$ 0.50");
+        Console.WriteLine($"{moeda25} moeda(s) de R$ 0.25");
+        Console.WriteLine($"{moeda10} moeda(s) de R$ 0.10");
+        Console.WriteLine($"{moeda05} moeda(s) de R$ 0.05");
+        Console.WriteLine($"{moeda01} moeda(s) de R$ 0.01");
+        */
+
+
+        /**
+        1020 - Idade em Dias
+        https://www.beecrowd.com.br/judge/pt/problems/view/1020
+
+        int tempoEmDias = int.Parse(Console.ReadLine());
+        int tempo = tempoEmDias;
+
+        int anos = tempo/365;
+        tempo -= anos*365;
+
+        int meses = tempo/30;
+        tempo -= meses*30;
+
+        int dias = tempo/1;
+        tempo -= dias*1;
+        
+        Console.WriteLine($"{anos} ano(s)");
+        Console.WriteLine($"{meses} mes(es)");
+        Console.WriteLine($"{dias} dia(s)");
+        */
+
+
+        /**
+        1019 - Conversão de Tempo
+        https://www.beecrowd.com.br/judge/pt/problems/view/1019
+
+        int tempoEmSegundos = int.Parse(Console.ReadLine());
+        int tempo = tempoEmSegundos;
+
+        int horas = tempo/3600;
+        tempo -= horas*3600;
+
+        int minutos = tempo/60;
+        tempo -= minutos*60;
+
+        int segundos = tempo/1;
+        tempo -= segundos*1;
+        
+        Console.WriteLine($"{horas}:{minutos}:{segundos}");
+        */
+
+
+        /**
+        1018 - Cédulas
+        https://www.beecrowd.com.br/judge/pt/problems/view/1018
+
+        int notasInseridas = int.Parse(Console.ReadLine());
+        int atual = notasInseridas;
+        
+        int notas100 = atual/100;
+        atual -= notas100*100;
+
+        int notas50 = atual/50;
+        atual -= notas50*50;
+
+        int notas20 = atual/20;
+        atual -= notas20*20;
+
+        int notas10 = atual/10;
+        atual -= notas10*10;
+
+        int notas5= atual/5;
+        atual -= notas5*5;
+
+        int notas2 = atual/2;
+        atual -= notas2*2;
+
+        int notas1 = atual/1;
+        atual -= notas1*1;
+
+        Console.WriteLine(notasInseridas);
+        Console.WriteLine($"{notas100} nota(s) de R$ 100,00");
+        Console.WriteLine($"{notas50} nota(s) de R$ 50,00");
+        Console.WriteLine($"{notas20} nota(s) de R$ 20,00");
+        Console.WriteLine($"{notas10} nota(s) de R$ 10,00");
+        Console.WriteLine($"{notas5} nota(s) de R$ 5,00");
+        Console.WriteLine($"{notas2} nota(s) de R$ 2,00");
+        Console.WriteLine($"{notas1} nota(s) de R$ 1,00");
+        */
+
+
         /**
         1017 - Gasto de Combustível
         https://www.beecrowd.com.br/judge/pt/problems/view/1017
