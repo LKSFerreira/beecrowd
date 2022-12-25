8ac730f26420f04f07235d6302d133f01fd66440
Lucas Ferreira <lks_ferreira@hotmail.com>
2022-12-25 18:36:51 -0300

    - Estudando
    Continuar depuração

---
+++ microsoft-learn/depuracao-interativa-dotnet/Program.cs
@@ -0,0 +1,19 @@
+﻿int result = Fibonacci(5);
+Console.WriteLine($"{result}");
+
+int Fibonacci(int numero)
+{
+   int penultimoNumero = 0;
+   int ultimoNumero = 1;
+   int somaDosNumeros = 0;
+
+   for (int i = 2; i < numero; i++)
+   {
+        somaDosNumeros = penultimoNumero + ultimoNumero;
+        penultimoNumero = ultimoNumero;
+        ultimoNumero = somaDosNumeros;
+   }
+
+   return somaDosNumeros;
+}
+
