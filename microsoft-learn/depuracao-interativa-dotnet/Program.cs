cd36c3f6bb1efb5d50add5edc49ed484f36d4d71
Lucas Ferreira <lks_ferreira@hotmail.com>
2022-12-26 05:01:10 -0300

    att

--- microsoft-learn/depuracao-interativa-dotnet/Program.cs
+++ microsoft-learn/depuracao-interativa-dotnet/Program.cs
@@ -1,19 +1,30 @@
-﻿int result = Fibonacci(5);
+﻿using System.Diagnostics;
+
+int result = Fibonacci(6);
 Console.WriteLine($"{result}");
 
 int Fibonacci(int numero)
 {
-   int penultimoNumero = 0;
-   int ultimoNumero = 1;
-   int somaDosNumeros = 0;
 
-   for (int i = 2; i < numero; i++)
-   {
+    Debug.WriteLine($"Entrando no metodo {nameof(Fibonacci)}");
+    Debug.WriteLine($"Estamos procurando pelo valor da sequência de posição {numero}");
+
+    int penultimoNumero = 0;
+    int ultimoNumero = 1;
+    int somaDosNumeros = 0;
+
+    for (int i = 2; i <= numero; i++)
+    {
         somaDosNumeros = penultimoNumero + ultimoNumero;
         penultimoNumero = ultimoNumero;
-        ultimoNumero = somaDosNumeros;
-   }
 
-   return somaDosNumeros;
+        Debug.WriteLineIf(somaDosNumeros == 1, $"A soma dos números é {somaDosNumeros}, o penultimo número é {penultimoNumero}, o último número é {ultimoNumero}");
+
+        ultimoNumero = somaDosNumeros;
+    }
+    Debug.Assert(ultimoNumero == 5, "O valor de retorno não é 5 e deveria ser.");
+
+    return numero == 0 ? penultimoNumero : ultimoNumero;
 }
 
+Console.WriteLine($"");
